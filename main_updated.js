2cbede27e4f2c88c07867c9ce2830b84a1cde1d9
Uvie Udjo <derek.ud123@gmail.com>
2024-12-05 13:16:56 -0400

    Add files via upload

---
+++ main_updated.js
@@ -0,0 +1,22 @@
+
+const showHideBtn = document.querySelector('.show-hide');
+const commentWrapper = document.querySelector('.comment-wrapper');
+
+commentWrapper.style.display = 'none';
+
+showHideBtn.onclick = function() {
+  let showHideText = showHideBtn.textContent;
+  if(showHideText === 'Show comments') {
+    showHideBtn.textContent = 'Hide comments';
+    commentWrapper.style.display = 'block';
+  } else {
+    showHideBtn.textContent = 'Show comments';
+    commentWrapper.style.display = 'none';
+  }
+};
+
+showHideBtn.onkeydown = function(e) {
+  if (e.key === "Enter") {
+    showHideBtn.click();
+  }
+};
